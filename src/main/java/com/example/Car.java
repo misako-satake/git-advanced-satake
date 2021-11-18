f7d468725a9de5e9d7ab94784c8f31922125272f
misako-satake <misako.satake@rakus-partners.co.jp>
2021-11-18 10:27:38 +0900

    Carクラスにフィールドを設定しました。

--- src/main/java/com/example/Car.java
+++ src/main/java/com/example/Car.java
@@ -1,5 +1,27 @@
 package com.example;
-
+/**
+ * 
+ * @author satakemisako
+ *
+ */
 public class Car {
-
+	private String name;
+	private int speed;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	@Override
+	public String toString() {
+		return "Car [name=" + name + ", speed=" + speed + "]";
+	}
+	
 }
